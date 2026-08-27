7eda14b2e38ca7d6255fcdd615e1ff1fa1a7dc81
Todd Skelton <todd.skelton@outlook.com>
2026-08-27 12:57:28 -0500

    feat(conformance): fix stable ISS-002 vector dispatch (#90)

--- packages/conformance/src/index.ts
+++ packages/conformance/src/index.ts
@@ -1,5 +1,6 @@
 export * from "./contracts.js";
 export * from "./candidate-materialization.js";
+export * from "./iss002-handler.js";
 export * from "./observation.js";
 export * from "./reducer.js";
 export * from "./stable.js";
