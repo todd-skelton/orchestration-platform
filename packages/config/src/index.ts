394f0b8e9bd897b65a25bfc1e1af1f9701974b85
Todd Skelton <todd.skelton@outlook.com>
2026-08-30 13:29:16 -0500

    fix(config): keep loader internal to source composition

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -1,11 +1,1 @@
 export { commandHandlerRegistration } from "./command-handler.mjs";
-export {
-  createConfigurationLoader,
-  createPortableConfigurationHostAdapter,
-  createWindowsConfigurationHostAdapter,
-  type ConfigurationHostAdapter,
-  type ConfigurationLoaderInvocation,
-  type ConfigurationLoadResult,
-  type LoadConfiguration,
-  type WindowsReparseFact,
-} from "./loader.js";
